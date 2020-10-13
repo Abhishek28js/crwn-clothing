16e6da0539c78534e4e22c8a34dced2178e44038
Abhishek jha <zhaavisek@gmail.com>
2020-10-13 16:21:25 +0545

    Added Homepage and styled the menu items

---
+++ src/pages/shop/shop-page.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './shop-page.styles.scss';
+
+class ShopPage extends React.Component{
+    contructor(props){
+        super(props);
+        this.state={
+            collections:
+        }
+    }
+
+    render(){
+        return(
+
+        )
+    }
+}
+export default ShopPage;
